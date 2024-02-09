d5d8dd638a167c75a29a657546eb53dc5dfc9577
Aryan <aryanmauryaofficial2601@gmail.com>
2024-02-10 03:30:07 +0530

    first page

---
+++ javascript/top_bottom_nav.js
@@ -0,0 +1,8 @@
+const bottom_nav = document.getElementById("nav_bottom")
+
+if(window.innerWidth > "768"){
+    bottom_nav.style.display = "none"
+}
+else{
+    bottom_nav.style.display = "block"
+}
